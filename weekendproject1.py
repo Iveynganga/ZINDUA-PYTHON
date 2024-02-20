96d4c9556077a88c0ac8f6fe2382825a525d7480
Iveynganga <wanjiruivey@gmail.com>
2024-02-20 23:16:14 +0300

    additional changes incorporating loops

--- weekendproject1.py
+++ weekendproject1.py
@@ -1,25 +1,47 @@
+import random
 
 options = ("Rock", "Paper", "Scissors")
 random= ("Rock", "Paper", "Scissors")
 
-user_choice = input("Choose Rock, Paper, or Scissors: ")
-computer_choice = input("Choose Random: ")
+user_score, computer_score = 0,0
+totalrounds = 3
+
+for i in range(totalrounds):
+    userOption = int(input('Press 1 for Rock, 2 for Paper, 3 for Scissors: '))
+    userChoice = options[userOption-1]
+
+    computerChoice = options[random(0,2)]
+
 
 print("You chose: ", user_choice)
 print("Computer chose: ", computer_choice)
 
-if user_choice == computer_choice :
-
-    print("it's a tie")
+if user_choice == computer_choice:
+        print("It's a tie!")
+        result =0    
 
 elif user_choice =="Rock" and computer_choice =="Scissors" :
     print("You've won")
+user_score += 1
 
-elif user_choice =="Paper" and computer_choice =="Rock" :
+if user_choice == "Paper" and computer_choice == "Rock":
     print("You've won")
+    user_score += 1
 
-elif user_choice =="Scissors" and computer_choice =="Paper" :
+elif user_choice == "Scissors" and computer_choice == "Paper":
     print("You've won")
+    user_score += 1
 
-else :
+else:
     print("Computer wins")
+
+print(f'Your score: {user_score}')
+print(f"Computer's score: {computer_score}")
+
+if user_score > computer_score:
+    print('You won most rounds!')
+
+elif computerscore > userscore:
+    print('Computer won most rounds!')
+else: 
+    print("It's a draw")
