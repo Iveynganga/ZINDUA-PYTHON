33da3ef62ec823e99fe948a0e8519d2632522d33
Iveynganga <wanjiruivey@gmail.com>
2024-03-12 13:04:47 +0300

    Rick and Morty Assignment

--- rickandmorty.py
+++ rickandmorty.py
@@ -1,32 +1,36 @@
 import requests
-import csv
 
-#fetching the number of characters
+# Fetching the number of characters
 def fetch_characters():
     url = "https://rickandmortyapi.com/api/character/"
-
-    characters = [20]
     response = requests.get(url)
+    
+    if response.status_code == 200:
+        characters = response.json()["info"]["count"]
+        print(f"Number of characters: {characters}")
+    else:
+        print(f"Failed to fetch characters. Status code: {response.status_code}")
 
-    print(characters)
-
-#fetching the number of locations
+# Fetching the number of locations
 def fetch_locations():
     url = "https://rickandmortyapi.com/api/location/"
-    
-
-    locations = [20]
     response = requests.get(url)
+    
+    if response.status_code == 200:
+        locations = response.json()["info"]["count"]
+        print(f"Number of locations: {locations}")
+    else:
+        print(f"Failed to fetch locations. Status code: {response.status_code}")
 
-    print(locations)
-
-#fetching the number of episodes
+# Fetching the number of episodes
 def fetch_episodes():
     url = "https://rickandmortyapi.com/api/episode/"
+    response = requests.get(url)
     
-    response = requests.get(url)
+    if response.status_code == 200:
+        episodes = response.json()["info"]["count"]
+        print(f"Number of episodes: {episodes}")
+    else:
+        print(f"Failed to fetch episodes. Status code: {response.status_code}")
 
-    episodes = [20]
-    response = requests.get(url)
-
-    print(episodes)
+    
