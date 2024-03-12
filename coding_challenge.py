531e4188e1d844965516283f60e42fa551a4c93a
Iveynganga <wanjiruivey@gmail.com>
2024-03-12 11:42:22 +0300

    Regular Expressions Assignment

--- coding_challenge.py
+++ coding_challenge.py
@@ -1,38 +1,27 @@
 import re
 
 def extract_phone_numbers(string):
-    phone_numbers = "(123) 456-7890 or (111) 222-3333"
-    print(extract_phone_numbers(string))
+    pattern = r"(\(\d{3}\)\s\d{3}-\d{4})"
+    return re.findall(pattern, string)
 
 def extract_email_addresses(string):
-    email_addresses = "Please contact info@example.com for assistance"
-    print(extract_email_addresses(string))
+    pattern = r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b"
+    return re.findall(pattern, string)
 
 def replace_email_addresses(string, replacement):
-    email_addresses = "Please contact info@example.com for assistance"
-    replacement = "REPLACED"
-    print(replace_email_addresses(string, "REPLACED"))
-
+    pattern = r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b"
+    return re.sub(pattern, replacement, string)
 
 phone_numbers = "(123) 456-7890, (111) 222-3333"
-pattern = r"(\(\d{3}\)\s\d{3}-\d{4}), (\(\d{3}\)\s\d{3}-\d{4})"
-          
-result = re.search(pattern, phone_numbers)
-
-print(result)
+print(extract_phone_numbers(phone_numbers))
 
 email_addresses = "Please contact info@example.com for assistance"
-pattern = r"\w{4}@\w{7}.com"
-result = re.search(pattern, email_addresses)
+print(extract_email_addresses(email_addresses))
 
-print(result)
-
-
-email_addresses = " 'Please contact info@example.com for assistance', 'replacement' "
-pattern = r"\w{4}@\w{7}.com"
+email_addresses_to_replace = "Please contact info@example.com for assistance"
 replacement = "REPLACED"
-result = re.search(pattern, replacement)
+print(replace_email_addresses(email_addresses_to_replace, replacement))
 
-print(result)
+
 
 
