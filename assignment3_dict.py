f76d0f375a8de2c6a36f9c2934592b72b1172c4b
Iveynganga <wanjiruivey@gmail.com>
2024-03-12 12:29:36 +0300

    Dictionaries Assignment

--- assignment3_dict.py
+++ assignment3_dict.py
@@ -1,22 +1,22 @@
 
 def find_highest_priced_product(products):
+    if not products:
+        return None
 
- 
-# Initial Dictionary
- my_dict =  [
+    # Use the max function with a lambda function as the key to find the dictionary with the highest price
+    highest_priced_product = max(products, key=lambda x: x["price"])
+
+    return highest_priced_product
+
+# Example Usage:
+products_list = [
     {"name": "Laptop", "price": 1200},
     {"name": "Smartphone", "price": 800},
     {"name": "Tablet", "price": 500},
     {"name": "Headphones", "price": 120},
 ]
-
-find_highest_priced_product(my_dict)
- 
-# Finding the highest price
- 
-print("product with highest price:")
-print(my_dict)
- 
+result = find_highest_priced_product(products_list)
+print(result)
  
 
  
